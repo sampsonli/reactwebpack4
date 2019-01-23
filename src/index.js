9a1bf735287a9e0e45c056f6342653432facda4d
sampson Li <740056710@qq.com>
2019-01-23 10:28:36 +0800

    fixbug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import 'core-js/shim';
+// import 'core-js/shim';
+import 'core-js/modules/es6.set';
+import 'core-js/modules/es6.map';
+import 'core-js/modules/es6.object.assign';
+import 'core-js/modules/es6.promise';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
