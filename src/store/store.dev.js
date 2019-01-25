2d23b460da386e975858e433fbb09e859f64e30f
sampson Li <740056710@qq.com>
2019-01-25 16:40:58 +0800

    optimate

--- src/store/store.dev.js
+++ src/store/store.dev.js
@@ -1,30 +1,18 @@
 // 如果是开发模式，store 采用此配置
 
 import {
-    createStore, applyMiddleware, compose, combineReducers,
+    createStore, applyMiddleware, compose,
 } from 'redux';
 import logger from 'redux-logger';
-import initReducers from '../reducers';
 import helper from './helper';
 
 const store = createStore(
-    combineReducers(initReducers),
+    () => {},
     window.__INITIAL_STATE__,
     compose(
         applyMiddleware(logger),
     ),
 );
-const asyncReducers = {
-    ...initReducers,
-};
+const asyncReducers = {};
 helper(store, asyncReducers);
-if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-        store.replaceReducer(combineReducers({
-            ...asyncReducers,
-        }));
-    });
-}
-
 export default store;
--- src/store/store.prod.js
+++ src/store/store.prod.js
@@ -1,15 +1,14 @@
 // 如果是产品（打包）模式，store 采用此配置
 
-import {createStore, combineReducers} from 'redux';
-import initReducers from '../reducers';
+import {
+    createStore,
+} from 'redux';
 import helper from './helper';
 
 const store = createStore(
-    combineReducers(initReducers),
+    () => {},
     window.__INITIAL_STATE__,
 );
-const asyncReducers = {
-    ...initReducers,
-};
+const asyncReducers = {};
 helper(store, asyncReducers);
 export default store;
