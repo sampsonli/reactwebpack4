f916cb65c7624bf6aeea51d98fb3b54fc1c8f0f2
sampson Li <740056710@qq.com>
2019-03-05 15:39:52 +0800

    test

--- src/routes/demo/pages/scroll/index.js
+++ src/routes/demo/pages/scroll/index.js
@@ -58,7 +58,7 @@ class ScrollDemo extends Component {
         return (
             <div className={`l-full l-flex-column ${style.wrapper}`}>
                 <div className={style.header}>
-                    新闻头条1
+                    <span>新闻头条2</span>
                 </div>
                 <div className="l-flex-1 l-relative">
                     <div className="l-full" id="wrapper">
