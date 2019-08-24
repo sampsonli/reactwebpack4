11aa7a10e10a18bb0b89e1d419f22188503c3796
sampson Li <740056710@qq.com>
2019-08-24 14:21:13 +0800

    fixbug

--- src/routes/demo/pages/scroll/index.js
+++ src/routes/demo/pages/scroll/index.js
@@ -1,11 +1,47 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import P from 'prop-types';
 
 import {observer} from 'mobx-react';
 import style from './style.less';
 import demo from '../../models/demo';
 
+export default observer(() => {
+    const [count, addCount] = useState(0);
+    useEffect(() => {
+        console.log(count, '变了');
+    }, [])
+    return (
+        <div className={`l-full l-flex-column ${style.wrapper}`}>
+            <div className={style.header} onClick={demo.setFriend}>
+                <span>
+                        新闻头条-33333--
+                    {demo.age}
+                </span>
+            </div>
+            <div className="l-flex-1 l-relative">
+                <div className="l-full" id="wrapper">
+                    {demo.friend && (
+                        <div onClick={demo.changeFriendName}>
+                            name:
+                            {demo.friend.name}
+                            <br />
+                            age:
+                            {demo.friend.age}
+                            <br />
+                            count:
+                            {count}
+                        </div>
+                    )}
+                    <button type="button" onClick={demo.addAge}>年龄+1</button>
+                    <button type="button" onClick={() => addCount(count + 1)}>数字+1</button>
+                </div>
 
+            </div>
+
+        </div>
+    );
+});
+/*
 @observer
 class ScrollDemo extends Component {
     componentDidMount() {
@@ -13,7 +49,6 @@ class ScrollDemo extends Component {
         console.log(location);
         console.log(history);
     }
-
     render() {
         return (
             <div className={`l-full l-flex-column ${style.wrapper}`}>
@@ -48,3 +83,4 @@ ScrollDemo.propTypes = {
     history: P.shape({replace: P.func, push: P.func}).isRequired,
 };
 export default ScrollDemo;
+*/
