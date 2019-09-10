39d30e443d17c2a91fcdffea57536c8ad3841f15
sampson Li <740056710@qq.com>
2019-09-10 17:55:33 +0800

    milestone+1+

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import 'core-js/modules/es.set';
 import 'core-js/modules/es.map';
 import 'core-js/modules/es.object.assign';
 import 'core-js/modules/es.promise';
+import 'core-js/modules/es6.symbol';
+import 'core-js/modules/es6.array.from';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './router';
