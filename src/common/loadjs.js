bf1590a68da1aa49adff3990734fcb91c26c4f85
lichun <lichun@mistong.com>
2020-06-23 15:26:12 +0800

    dev

--- src/common/loadjs.js
+++ src/common/loadjs.js
@@ -2,20 +2,19 @@ export default (src, attr = '') => {
     if (!window.__loaded) {
         window.__loaded = {};
     }
-    return new Promise((resolve, reject) => {
-        if (!window.__loaded[src]) {
-            const element = document.createElement('script');
-            document.body.appendChild(element);
-            element.src = src;
-            element.onload = () => {
-                window.__loaded[src] = true;
-                resolve(attr && window[attr]);
-            };
-            element.onerror = (e) => {
-                reject(e);
-            };
-        } else {
+    if (window.__loaded[src]) {
+        return window.__loaded[src];
+    }
+    window.__loaded[src] = new Promise((resolve, reject) => {
+        const element = document.createElement('script');
+        document.body.appendChild(element);
+        element.src = src;
+        element.onload = () => {
             resolve(attr && window[attr]);
-        }
+        };
+        element.onerror = (e) => {
+            reject(e);
+        };
     });
+    return window.__loaded[src];
 };
