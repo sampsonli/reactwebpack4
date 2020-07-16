69778216efa19ce828cd00eac139811938fa0ae4
lichun <lichun@mistong.com>
2020-07-16 14:15:13 +0800

    optimate

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import 'core-js/modules/es.promise';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import deliverer from 'react-deliverer';
-import VConsole from 'vconsole';
-
 import { Provider } from 'react-redux';
+import loadjs from '~/common/loadjs';
 import Router from './router';
 import store from './store';
 
 import './assets/common.css';
 
 deliverer(store);
-if (process.env.NODE_ENV === 'development') {
-    window._vc = new VConsole();
+if (window.location.href.indexOf('debug=1') > -1) {
+    loadjs('lib/vconsole.min.js', 'VConsole').then(VConsole => {
+        window._vc = new VConsole();
+    });
 }
 ReactDOM.render(
     <Provider store={store}>
