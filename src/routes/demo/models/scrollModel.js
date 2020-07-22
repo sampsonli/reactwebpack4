ac53a66ce89e48b630f598b7d0fb0cf46e5d5578
lichun <lichun@mistong.com>
2020-07-22 20:18:50 +0800

    fixbug

--- src/routes/demo/models/scrollModel.js
+++ src/routes/demo/models/scrollModel.js
@@ -9,13 +9,25 @@ class ScrollModel {
         this.#loading = false;
     }
 
+    testClick() {
+        const a = {b: {c: 1}};
+        console.log(a.b ?? 'hello');
+    }
+
     initScroll(el) {
         if (el) {
             const target = el.querySelector('*');
+            const headBar = el.querySelector('#headBar');
             target.style.willChange = 'transform';
+            headBar.style.willChange = 'transform';
             const transform = typeof target.style.transform !== 'undefined' ? 'transform' : 'webkitTransform';
             const scroller = new Scroll((left, top) => {
                 target.style[transform] = `translateY(-${parseInt(top, 10)}px) translateZ(0)`;
+                if (top > 100) {
+                    headBar.style[transform] = `translateY(${parseInt(top - 100, 10)}px) translateZ(0)`;
+                } else {
+                    headBar.style[transform] = 'translateY(0) translateZ(0)';
+                }
             }, {
                 scrollingX: false,
                 scrollingY: true,
